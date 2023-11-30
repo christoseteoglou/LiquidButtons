21a193c56db36be3ffd231cd9547d354feb5d837
christoseteoglou <itsmeglue@gmail.com>
2023-11-30 09:43:11 +0100

    Initial Commit

---
+++ LiquidButton/LiquidButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidButtonApp.swift
+//  LiquidButton
+//
+//  Created by Christos Eteoglou on 2023-11-30.
+//
+
+import SwiftUI
+
+@main
+struct LiquidButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
